8d6f9eea263e9394ee693a7a5826cae70047f97b
Poorya <110911383+pooryas98@users.noreply.github.com>
2022-09-15 21:12:10 +0430

    Update what_episode_to_watch.py

--- what_episode_to_watch.py
+++ what_episode_to_watch.py
@@ -6,5 +6,5 @@ episode = int(input("how many episodes? (only integer numbers are valid)"))
 season_list = np.arange(season)
 episode_list = np.arange(episode)
 
-print("Season = {}".format(np.random.choice(season_list)))
-print("Episode = {}".format(np.random.choice(episode_list)))
+print("Season = {}".format(np.random.choice(season_list) + 1))
+print("Episode = {}".format(np.random.choice(episode_list) + 1))
