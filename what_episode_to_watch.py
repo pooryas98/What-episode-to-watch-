78d3570cfef3eb3e1a97f8faf8e02b9c02517c29
Poorya <110911383+pooryas98@users.noreply.github.com>
2022-09-15 21:03:20 +0430

    Add files via upload

---
+++ what_episode_to_watch.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+season = int(input("how many seasons? (only integer numbers are valid)"))
+episode = int(input("how many episodes? (only integer numbers are valid)"))
+
+season_list = np.arange(season)
+episode_list = np.arange(episode)
+
+print("Season = {}".format(np.random.choice(season_list)))
+print("Episode = {}".format(np.random.choice(episode_list)))
